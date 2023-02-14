0283f9acbfb81aa978130a0707587c63847ce584
Ahwx <ahwx@ahwx.org>
2023-02-14 13:06:49 +0100

    index page is ready

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 <body>
     <div class="mainContainer">
         <h1 id="bodyHeader"><span>P</span>inter<span>n</span>et</h1>
-        <p class="bodyText">Please enter the Pin ID here:</p>
+        <form class="searchContainer" action="search.php" method="get" autocomplete="off">
+          <div id="inputWrapper"><br><br><br><br>
+            <input type="text" name="q" autofocus/>
+            <button type="submit">Search</button>
+          </div>
+        </form>
     </div>
 <?php require "static/footer.php"; ?>
--- static/footer.php
+++ static/footer.php
@@ -1,6 +1,12 @@
-<div class="footer">
-  <a href="./api.php" target="_blank">API</a>
-  <a href="https://github.com/Ahwxorg/Pinternet/" target="_blank">Source code</a>
-</div>
+<footer>
+  <div class="footer">
+    <p id="footerText">
+      <!--<a href="./api.php" target="_blank">API</a>-->
+      |
+      <a href="https://github.com/Ahwxorg/Pinternet/" target="_blank">Source code</a>
+      |
+    </p>
+  </div>
+</footer>
 </body>
 </html>
