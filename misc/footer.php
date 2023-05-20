bd76902f73125c95ee5223748d832ece7764ac91
Ahwxorg <jaap@jaapvd.nl>
2023-05-20 14:40:10 +0200

    Add legal notice, add blank lines before footer.

--- misc/footer.php
+++ misc/footer.php
@@ -4,17 +4,18 @@
       |
       <a href="./" target="_blank">Pinternet</a>
       |
-      <a href="https://github.com/Ahwxorg/Pinternet/" target="_blank">Source code</a>
+      <a href="https://github.com/Ahwxorg/pinternet/" target="_blank">Source code</a>
       |
       <a href="https://ahwx.org/donate.php" target="_blank">Donate</a>
       |
+      <a href-"https://github.com/Ahwxorg/pinternet#legal-notice" target="_blank">Legal notice</a>
 
 <?php
 if (isset($images)) {
   echo "<br><br>";
   print(count($images). " images found");
 }
-?>
+?><br><br>
     </p>
   </div>
 </footer>
