26bfc87269e35304944d506793bdabf3c63d62b1
Ahwx <jaap@jaapvd.nl>
2023-09-03 00:29:37 +0200

    fix(misc/tools.php): fix vuln

--- misc/tools.php
+++ misc/tools.php
@@ -1,13 +1,6 @@
 <?php
-    function get_root_domain($url)
-    {
-        $split_url = explode("/", $url);
-        $base_url = $split_url[2];
-
-        $base_url_main_split = explode(".", strrev($base_url));
-        $root_domain = strrev($base_url_main_split[1]) . "." . strrev($base_url_main_split[0]);
-    
-        return $root_domain;
+    function get_root_domain($url) {
+      return parse_url($url, PHP_URL_HOST);
     }
 
     function request($url)
