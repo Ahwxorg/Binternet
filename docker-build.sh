2a3ab3a19eefbdb15da281176b35a8d3b85cfb12
Revvy <nothingtohide@revvy.de>
2023-10-09 07:38:55 -0400

    make docker better

--- docker-build.sh
+++
@@ -1,5 +0,0 @@
-echo Version?
-read -r VERSION
-docker build -t binternet:$VERSION . && docker tag binternet:$VERSION ahwx/binternet:$VERSION && docker push ahwx/binternet:$VERSION
-
-docker build -t binternet:latest . && docker tag binternet:latest ahwx/binternet:latest && docker push ahwx/binternet:latest
