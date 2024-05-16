087003b134e5579d47eed6ae07f1c85a602affc0
Danii Saahir <158811885+danii-saahir@users.noreply.github.com>
2024-05-17 00:08:23 +0800

    Update image_proxy.php
    1. Add support for "pin.it".
    2. Improved error handling with appropriate HTTP response codes and error messages.
    3. Utilized `filter_var` for URL sanitization.

--- image_proxy.php
+++ image_proxy.php
@@ -1,19 +1,26 @@
 <?php
-
 require "misc/tools.php";
-
-$url = $_REQUEST["url"];
+$url = filter_var($_REQUEST["url"], FILTER_SANITIZE_URL);
 $requested_root_domain = get_root_domain($url);
-
-$allowed_domains = array("pinimg.com", "i.pinimg.com", "pinterest.com");
-
-if (in_array($requested_root_domain, $allowed_domains))
-{
-  $image = $url;
-  $image_src = request($image);
-
-  header("Content-Type: image/png");
-  echo $image_src;
+$allowed_domains = array("pinimg.com", "i.pinimg.com", "pinterest.com", "pin.it");
+if (in_array($requested_root_domain, $allowed_domains)) {
+    $image_src = request($url);
+    if ($image_src !== false) {
+        $finfo = new finfo(FILEINFO_MIME_TYPE);
+        $mime_type = $finfo->buffer($image_src);
+        if (strpos($mime_type, 'image/') === 0) {
+            header("Content-Type: $mime_type");
+            echo $image_src;
+        } else {
+            header("HTTP/1.1 415 Unsupported Media Type");
+            echo "Error: The requested URL does not contain a valid image.";
+        }
+    } else {
+        header("HTTP/1.1 404 Not Found");
+        echo "Error: Unable to fetch the image.";
+    }
+} else {
+    header("HTTP/1.1 400 Bad Request");
+    echo "Error: Invalid domain.";
 }
-
 ?>
