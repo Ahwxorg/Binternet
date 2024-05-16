b30dbe21a1ead4f736827178bb329bcdbf31ac3a
Danii Saahir <158811885+danii-saahir@users.noreply.github.com>
2024-05-17 00:12:43 +0800

    Update tools.php
    1. Added proper closure of cURL handle after execution.

--- misc/tools.php
+++ misc/tools.php
@@ -1,15 +1,13 @@
 <?php
-    function get_root_domain($url) {
-      return parse_url($url, PHP_URL_HOST);
-    }
-
-    function request($url)
-    {
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_HEADER, false);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      $response = curl_exec($ch);
-
-      return $response;
-    }
+function get_root_domain($url) {
+    return parse_url($url, PHP_URL_HOST);
+}
+function request($url) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
 ?>
