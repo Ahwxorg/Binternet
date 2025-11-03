34bd76d54d2810b6e2c4c7352bf9e9fdc99d3842
Axel <74874782+notthewave@users.noreply.github.com>
2025-11-03 08:47:37 +0100

    disable zoom on input for mobile devices

--- misc/header.php
+++ misc/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html >
 <html lang="en">
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
     <meta charset="UTF-8"/>
     <meta name="description" content="A privacy respecting Pinterest image search"/>
     <meta name="referrer" content="no-referrer"/>
